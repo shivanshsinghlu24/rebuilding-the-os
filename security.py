a13fd7c52ef898d9453974f7c1cd8061e02405e9
Shivansh Singh <shivanshsinghlu24@gmail.com>
2026-03-27 16:33:43 +0530

    Update security.py

--- security.py
+++ security.py
@@ -1,1 +1,7 @@
+def is_safe(command):
+    dangerous = ["rm -rf", "del /f", "shutdown", "format"]
 
+    for cmd in dangerous:
+        if cmd in command.lower():
+            return False
+    return True
