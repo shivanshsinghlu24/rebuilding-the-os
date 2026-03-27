ec94a36c45868eda9be71231fa6ed70e20209d04
Shivansh Singh <shivanshsinghlu24@gmail.com>
2026-03-27 16:33:21 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,1 +1,7 @@
+import psutil
 
+def show_system_stats():
+    print("\n--- System Stats ---")
+    print("CPU Usage:", psutil.cpu_percent(), "%")
+    print("Memory Usage:", psutil.virtual_memory().percent, "%")
+    print("--------------------\n")
