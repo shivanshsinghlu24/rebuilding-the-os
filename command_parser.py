40dc905f3b6d025127963c96ca1f8ae9a2653bcc
Shivansh Singh <shivanshsinghlu24@gmail.com>
2026-03-27 16:32:36 +0530

    Update command_parser.py

--- command_parser.py
+++ command_parser.py
@@ -1,1 +1,14 @@
+def parse_command(user_input):
+    mappings = {
+        "show files": "dir",
+        "list files": "dir",
+        "current directory": "cd",
+        "make folder": "mkdir",
+        "remove file": "del"
+    }
 
+    for key in mappings:
+        if key in user_input.lower():
+            return mappings[key]
+
+    return user_input
