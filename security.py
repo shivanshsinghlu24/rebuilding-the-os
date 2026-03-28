f31f21e39c21f1e47a7690560e024fb30c42665c
Shivansh Singh <shivanshsinghlu24@gmail.com>
2026-03-28 12:29:00 +0530

    Delete security.py

--- security.py
+++
@@ -1,7 +0,0 @@
-def is_safe(command):
-    dangerous = ["rm -rf", "del /f", "shutdown", "format"]
-
-    for cmd in dangerous:
-        if cmd in command.lower():
-            return False
-    return True
