c2d27422c32aeaf36e2f9fc6d1362a7fd802e2db
Shivansh Singh <shivanshsinghlu24@gmail.com>
2026-03-28 12:29:24 +0530

    Delete monitor.py

--- monitor.py
+++
@@ -1,7 +0,0 @@
-import psutil
-
-def show_system_stats():
-    print("\n--- System Stats ---")
-    print("CPU Usage:", psutil.cpu_percent(), "%")
-    print("Memory Usage:", psutil.virtual_memory().percent, "%")
-    print("--------------------\n")
